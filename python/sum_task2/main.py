dc0a366b21f3656f89640daf3029ba183bf060cc
Taiisia Shevchenko <taiisia.shevchenko@gmail.com>
2022-05-20 20:52:36 +0300

    Sum and number out of the range

---
+++ python/sum_task2/main.py
@@ -0,0 +1,24 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def sum_number(a, b):
+    total = 0
+    cnt = 0
+    for i in range (1, 7):
+        print("Enter the number")
+        input_a = input()
+        input_a = int(input_a)
+        if input_a > b or input_a < a:
+            cnt += 1
+            total += input_a
+
+    print(f"total: {total} number: {cnt}")
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    sum_number(5, 10)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
